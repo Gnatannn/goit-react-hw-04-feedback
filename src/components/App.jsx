ff17d6cc89de06928238ece71ac30c5caa8804e5
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-10-01 21:45:11 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,6 @@
-export const App = () => {
+import { Component } from 'react';
+
+export class App extends Component {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  )
 };
