ee8d4402ac9923af6c668cd00dd3145f9305d2f9
Gnatannn <96788997+Gnatannn@users.noreply.github.com>
2022-10-17 19:24:56 +0300

    1

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,33 +1,39 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Notiflix from 'notiflix';
 import { Container } from './Feedback.styled';
 import { Section } from 'components/Section/Section';
 import { FeedbackOptions } from 'components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from 'components/Statistics/Statistics';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+export const Feedback = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleStateClick = feedback => {
+  const handleStateClick = feedback => {
     Notiflix.Notify.success(`Thank you for a ${feedback} feedback!`);
-    this.setState(prevState => ({
-      [feedback]: prevState[feedback] + 1,
-    }));
+    switch (feedback) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        Notiflix.Notify.warning(`What is your ${feedback} feedback???`);
+        break;
+    }
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => {
-      return (acc += value);
-    }, 0);
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    const { good } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     if (good !== 0) {
       return Math.round((good * 100) / total);
     } else {
@@ -35,32 +41,32 @@ class Feedback extends Component {
     }
   };
 
-  render() {
-    return (
-      <>
-        <Container>
-          <Section title="Please leave a feedback">
-            <FeedbackOptions
-              options={Object.keys(this.state)}
-              onClickFeedback={this.handleStateClick}
+  const options = ['good', 'neutral', 'bad'];
+
+  return (
+    <>
+      <Container>
+        <Section title="Please leave a feedback">
+          <FeedbackOptions
+            options={options}
+            onClickFeedback={handleStateClick}
+          />
+          {countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positiveRate={countPositiveFeedbackPercentage()}
             />
-            {this.countTotalFeedback() > 0 ? (
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.countTotalFeedback()}
-                positiveRate={this.countPositiveFeedbackPercentage()}
-              />
-            ) : (
-              <h2 style={{ textAlign: 'center' }}>There is no feedback</h2>
-            )}
-          </Section>
-        </Container>
-      </>
-    );
-  }
-}
+          ) : (
+            <h2 style={{ textAlign: 'center' }}>There is no feedback</h2>
+          )}
+        </Section>
+      </Container>
+    </>
+  );
+};
 
 Notiflix.Notify.init({
   position: 'right-bottom',
